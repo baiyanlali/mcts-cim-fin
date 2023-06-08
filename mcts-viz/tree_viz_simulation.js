b04cd89e9a4d521acf19c3412d47a310586e0e35
Zhao Yunlong <zao44457660@126.com>
2023-06-08 19:59:53 +0800

    修改了四个步骤中的winner的名称

--- mcts-viz/tree_viz_simulation.js
+++ mcts-viz/tree_viz_simulation.js
@@ -5,6 +5,113 @@ const Player = {
     1: "Player B"
 }
 
+export const vis_four = (s) => {
+
+    vis(s)
+
+    let node_size = {x: 50, y: 55};
+
+    const show_txt = node_size.x * 1.8
+
+
+    s.drawNode = (board, value, visits, node) => {
+        if (node.data.simulated_board) {
+            board = node.data.simulated_board;
+        }
+
+        let tile_size = node_size.x / 3;
+
+
+        if (node.id == s.hovered_node_id) {
+            s.fill(200);
+        } else {
+            s.fill(255)
+        }
+
+        if (node.data.best_move) {
+            s.strokeWeight(2);
+            s.stroke(255, 0, 0);
+        }
+
+        if (s.show_simple) {
+            s.rect(0, 0, node_size.x, node_size.y, 5);
+        } else {
+            s.rect(0, 0, node_size.x + show_txt, node_size.y, 5);
+        }
+
+
+        if (node.data.best_move) {
+            s.strokeWeight(0.5);
+            s.stroke(0);
+        }
+
+        s.drawGame(board)
+
+
+        s.push();
+        s.textSize(tile_size * 0.8);
+
+        // s.translate(0, (node_size.y - node_size.x) / 10);
+        s.translate(node_size.x, 0);
+
+        const text_width = 45
+
+        value = value.toFixed(1)
+        if (!node.isRoot()) {
+            let uct = UCB1(node, s.tree.getParent(node)).toFixed(2);
+            if (isNaN(uct)) {
+                uct = "--";
+            }
+
+
+            if (node === s.hovered_node) {
+                node.uct_txt = uct
+                node.vis_txt = visits
+                node.val_txt = value
+            }
+
+            if (!s.show_simple) {
+
+                s.textAlign(s.LEFT, s.TOP);
+                s.text(" val: ", 0, 0);
+                s.text(" vis: ", 0, (node_size.y) / 4);
+                s.text(" uct: ", 0, 2 * (node_size.y) / 4);
+                s.textAlign(s.RIGHT, s.TOP);
+                s.text("  " + value + " ", text_width + node_size.x, 0);
+                s.text("  " + visits + " ", text_width + node_size.x, (node_size.y) / 4);
+                s.text("  " + uct + " ", text_width + node_size.x, 2 * (node_size.y) / 4);
+            }
+        } else {
+            if (node === s.hovered_node) {
+                node.vis_txt = visits
+                node.val_txt = value
+            }
+            if (!s.show_simple) {
+
+                s.textAlign(s.LEFT, s.TOP);
+                s.text(" val: ", 0, 0);
+                s.text(" vis: ", 0, (node_size.y) / 4);
+                s.textAlign(s.RIGHT, s.TOP);
+                s.text("  "+value + " ", text_width + node_size.x, 0);
+                s.text("  " + visits + " ", text_width + node_size.x, (node_size.y) / 4);
+            }
+
+            // s.textAlign(s.LEFT, s.CENTER);
+            // s.text(" vis:", 0, 0);
+            // s.textAlign(s.RIGHT, s.CENTER);
+            // s.text(visits + " ", 0, 0);
+        }
+
+
+        s.pop();
+
+        if (node.data.simulated_board) {
+            let winner_icon = node.data.simulated_board.checkWin();
+            s.text(winner_icon === "v" ? "DRAW" : (" WINNER: " + Player[node.data.move.player]), 0, node_size.y * 1.25);
+        }
+    }
+}
+
 export const vis_simulation = (s) => {
     vis(s)
 
